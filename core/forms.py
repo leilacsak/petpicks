239fc3efa82aee34a5e6ff4e3804307ae11273a4
leilacsak <leilacsak@gmail.com>
2026-02-22 14:28:57 +0000

    Fix Devtool  issues: ContactForm fields to autocomplete attributes

--- core/forms.py
+++ core/forms.py
@@ -2,10 +2,19 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True, label="Your Name")
-    email = forms.EmailField(required=True, label="Your Email")
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+        label="Your Name",
+        widget=forms.TextInput(attrs={"autocomplete": "name"})
+    )
+    email = forms.EmailField(
+        required=True,
+        label="Your Email",
+        widget=forms.EmailInput(attrs={"autocomplete": "email"})
+    )
     message = forms.CharField(
-        widget=forms.Textarea(attrs={"rows": 4}),
+        widget=forms.Textarea(attrs={"rows": 4, "autocomplete": "message"}),
         required=True,
         label="Message"
     )
