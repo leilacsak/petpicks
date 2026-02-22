feb30f1981df747464aacf6b32010b157ae30d42
leilacsak <leilacsak@gmail.com>
2026-02-22 13:06:22 +0000

    Add contact link to navigation bar and implement redirecting

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
+
+
+# Redirect view for /contact/ to home with anchor
+
+def contact_redirect(request):
+    return redirect('/#contact-form')
+
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("contact/", contact_redirect, name="contact"),
 ]
